03b741319e8a84226d5841aa6516fd92676a7667
Trevor Carmack <tscarmack77@gmail.com>
2024-03-14 19:03:50 -0500

    added remove first and last character

--- objects-nasa/js/main.js
+++ objects-nasa/js/main.js
@@ -1,20 +1,23 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
-//Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
 
-function getFetch(){
+function getFetch() {
   const choice = document.querySelector('input').value
   const url = `https://api.nasa.gov/planetary/apod?api_key=P4hwVxbxw8QyWdIWrCya54lwzgJLb8KhkYerGoUO&date=${choice}`
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-        document.querySelector('img').src = data.hdurl
-        document.querySelector('h3').innerText= data.explanation
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data);
+      document.querySelector('img').src = data.hdurl;
+      if (data.explanation !== undefined) {
+        document.querySelector('h3').innerText = data.explanation;
+      } else {
+        document.querySelector('h3').innerText = data.msg;
+      }
+    })
+    .catch(err => {
+      console.log(`error ${err}`);
+    });
+  
 }
-
